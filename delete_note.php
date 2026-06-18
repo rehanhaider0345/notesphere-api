92b7709e5d9c7904bc659cec23da77297a978cce
Syed Rehan Haider <152317604+rehanhaider0345@users.noreply.github.com>
2026-06-18 16:59:42 +0500

    Remove duplicate db.php include in delete_note.php
    Removed redundant include statement for db.php.

--- delete_note.php
+++ delete_note.php
@@ -1,11 +1,9 @@
 <?php
-
+require_once "db.php";
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
-
-include "db.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
 
